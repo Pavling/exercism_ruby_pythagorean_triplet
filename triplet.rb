8c1f474bc00251fa02f87b60fdd34ebe0e2ea125
Michael Pavling <pavling@gmail.com>
2013-11-06 16:10:25 +0000

    passing test triplets up to 10

--- triplet.rb
+++ triplet.rb
@@ -1,5 +1,21 @@
 class Triplet
 
+  def self.where(params={})
+    {max_factor: 10}.merge(params)
+    max_factor = params.fetch(:max_factor)
+    
+    triplets = []
+    (1..max_factor).each do |factor1|
+      (1..max_factor).each do |factor2|
+        (1..max_factor).each do |factor3|
+          triplets << [factor1, factor2, factor3].sort
+        end
+      end
+    end
+    triplets = triplets.uniq.select {|triplet| Triplet.new(*triplet).pythagorean?}
+    triplets.map {|triplet| Triplet.new(*triplet)}
+  end
+
   def initialize(a, b, c)
     @sides = [a, b, c].sort
   end
